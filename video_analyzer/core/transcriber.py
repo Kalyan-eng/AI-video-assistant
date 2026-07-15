2b0b4de026912583b11d46dc6ab7a86ad3311619
Kalyan <kalyanpasula33@gmail.com>
2026-07-15 11:12:08 +0530

    Add files via upload

---
+++ video_analyzer/core/transcriber.py
@@ -0,0 +1,35 @@
+import whisper
+import os
+
+WHISPER_MODEL = os.getenv("WHISPER_MODEL","small")
+
+_model = None
+
+def load_model() :
+
+    global _model
+
+    if _model is None :
+        print("Loading model ...")
+        _model = whisper.load_model(WHISPER_MODEL)
+        print("Whisper model loaded successfully")
+
+    return _model
+
+def transcribe_chunk(chunk_path:str , translate : bool= False) -> str :
+    model = load_model()
+    task =  "translate" if translate else "transcribe"
+    result = model.transcribe(chunk_path, task = task)
+    return result['text']
+
+def transcribe_all (chunks : list , translate:bool = False) -> str :
+    full_transcript =  " "
+
+    for i, chunk in enumerate(chunks) :
+        print(f"transcribing chunks {i+1}") 
+        text = transcribe_chunk(chunk, translate = translate)
+        full_transcript += text + " "
+        
+    print("Transcription completed successfully")
+    return full_transcript
+
